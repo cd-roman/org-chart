e2bf372253bd9f18c2a8a424e6fbb31fcc6d3584
Roman <60399380+cd-roman@users.noreply.github.com>
2024-03-26 04:31:16 +0100

    Add generate-react-cli and templates

---
+++ src/generate-react-cli-templates/TemplateName.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TemplateNameWrapper } from "./TemplateName.styles.jsx";
+
+const TemplateName = () => (
+  <TemplateNameWrapper>TemplateName Component</TemplateNameWrapper>
+);
+
+TemplateName.propTypes = {};
+
+TemplateName.defaultProps = {};
+
+export default TemplateName;
